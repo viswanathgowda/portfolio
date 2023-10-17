ce33bb2298db5539f2310134dde6810ba10d4d42
viswanathgowda <viswavishu40@gmail.com>
2023-10-17 23:45:00 +0530

    responsive added to skills page

--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -9,23 +9,23 @@ import data from '../data/data.json'
 export class SkillsComponent implements OnInit {
   skills = data.aboutme.skills
  
-  skillSet: any;
+  skillSets: any;
   nodataFound: any;
   constructor() { }
 
   ngOnInit(): void {
-    this.skillSet = this.skills.languages[0].web
+    this.skillSets = this.skills.languages[0].web
   }
 
   skillset(set: any) {
-    if(set == 'web'){
-      this.skillSet = this.skills.languages[0].web
+    if(set == 1){
+      this.skillSets = this.skills.languages[0].web
       this.nodataFound = this.skills.languages[0].web?.length
-    } else if(set == 'backend'){
-      this.skillSet = this.skills.languages[1].backend
+    } else if(set == 2){
+      this.skillSets = this.skills.languages[1].backend
       this.nodataFound = this.skills.languages[1].backend?.length
-    } else if(set == 'testing'){
-      this.skillSet = this.skills.languages[2].testing
+    } else if(set == 3){
+      this.skillSets = this.skills.languages[2].testing
       this.nodataFound = this.skills.languages[2].testing?.length
     }
   }
