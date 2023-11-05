16c108fddf6ad3852299d3adb76c2f8ae84ee0ce
viswanathgowda <viswavishu40@gmail.com>
2023-11-05 16:17:46 +0530

    adding contact page

--- src/app/contactpage/contactpage.component.ts
+++ src/app/contactpage/contactpage.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Loader } from '@googlemaps/js-api-loader';
+import { styles } from '../mapstyles';
+import * as data from '../data/data.json'
+
 
 @Component({
   selector: 'app-contactpage',
@@ -7,23 +11,97 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactpageComponent implements OnInit {
 
-  // private map!: google.maps.Map;
-  // location = {
-  //   'lat': 12.916576,
-  //   'lang': 77.610115
-  // }
-  // center: google.maps.LatLngLiteral = { lat: 51.5074, lng: -0.1278 };
-  // zoom = 10;
-  // markerPosition: google.maps.LatLngLiteral = { lat: 51.5074, lng: -0.1278 };
-  // infoWindowPosition: google.maps.LatLngLiteral = { lat: 51.5074, lng: -0.1278 };
-  // infoWindowOpened = false;
-
+  resume = data;
+  arrlglt = [
+    {
+      icon: 'assets/young-bearded-man-with-striped-shirt.jpg',
+      title: 'Its me Vishwa.',
+      latd: 12.90601299971035,
+		  langtd: 77.60589159417181,
+    },
+    {
+      icon: 'assets/icons8-company-48.jpg',
+      title: 'Employer/company',
+      latd:  16.5833,
+		  langtd: 82.0167
+    },  
+  ]
+ 
+  private map!: google.maps.Map;
+  latitude: any;
+  longitude: any;
+  like: boolean = false;
   constructor() { }
 
   ngOnInit(): void {
-    this.googleMap()
+    this.getcurrentLocation()
+    this.like = false;
   }
 
-  googleMap(){
+  getcurrentLocation(){
+    navigator.geolocation.getCurrentPosition((position) =>{
+        this.latitude = position.coords.latitude;
+        const latd = parseFloat(this.latitude);
+        this.longitude = position.coords.longitude;
+        // this.langtd = parseFloat(this.longitude)
+        console.log(position, this.latitude, latd)
+        let l = {...this.arrlglt[1], latd : this.latitude, langtd: this.longitude}
+        this.arrlglt[1] = l
+        l ? this.googleMap() : ''
+    })
   }
+  googleMap(){
+    let loader = new Loader({
+      apiKey: 'AIzaSyCtfYgcatV9vr5B5LhVWFE_2GFgI2IRU_Q'
+    })
+    loader.load().then(() => {
+      console.log(this.arrlglt)
+      this.map = new google.maps.Map(document.getElementById("map")!, {
+        center: { lat: this.arrlglt[0].latd, lng: this.arrlglt[0].langtd },
+        zoom: 16,
+        styles: styles
+      })
+      if(this.arrlglt.length){
+        for(let l of this.arrlglt){
+          const marker = new google.maps.Marker({
+            position: new google.maps.LatLng(l.latd, l.langtd),
+            map: this.map,
+            title: l.title,
+            icon: {
+            url: l.icon,
+            scaledSize: new google.maps.Size(36, 36),
+            /**
+             * The position at which to anchor an image in correspondence to the
+             * location of the marker on the map. By default, the anchor is located
+             * along the center point of the bottom of the image.
+             */
+            anchor: new google.maps.Point(18, 18),
+            },           
+          });
+          // Create an info window to share between markers.
+          const infoWindow = new google.maps.InfoWindow();
+          marker.addListener("click", () => {
+            infoWindow.close();
+            infoWindow.setContent(marker.getTitle());
+            infoWindow.open(marker.getMap(), marker);
+          });
+        }
+      }
+    })
+    
+  }
+  share(){
+    const url = (window.location.href).toString()
+    const data = {
+      title: 'portfolio',
+      text: 'viswanath',
+      url: url
+    }
+    if(navigator.canShare(data) && navigator.canShare()){
+      navigator.share(data)
+    }else{
+      alert('sharing not supported for this device')
+    }
+  }
+
 }
