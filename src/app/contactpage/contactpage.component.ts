bebfc71ade82612f557b322df5efbd78c20f6b73
viswanathgowda <viswavishu40@gmail.com>
2024-03-13 12:13:25 +0530

    format changes

--- src/app/contactpage/contactpage.component.ts
+++ src/app/contactpage/contactpage.component.ts
@@ -1,114 +1,113 @@
 import { Component, OnInit } from '@angular/core';
 import { Loader } from '@googlemaps/js-api-loader';
 import { styles } from '../mapstyles';
-import * as data from '../data/data.json'
+import * as data from '../data/data.json';
 import { DataService } from '../data.service';
-
 
 @Component({
   selector: 'app-contactpage',
   templateUrl: './contactpage.component.html',
-  styleUrls: ['./contactpage.component.css']
+  styleUrls: ['./contactpage.component.css'],
 })
 export class ContactpageComponent implements OnInit {
-  resume: any
+  resume: any;
   arrlglt = [
     {
       icon: 'assets/young-bearded-man-with-striped-shirt.jpg',
       title: 'Its me Vishwa.',
       latd: 12.90601299971035,
-		  langtd: 77.60589159417181,
+      langtd: 77.60589159417181,
     },
     {
       icon: 'assets/icons8-company-48.jpg',
       title: 'Employer/company',
-      latd:  16.5833,
-		  langtd: 82.0167
-    },  
-  ]
- 
+      latd: 16.5833,
+      langtd: 82.0167,
+    },
+  ];
+
   private map!: google.maps.Map;
   latitude: any;
   longitude: any;
   like: boolean = false;
-  constructor(private rdata: DataService) { }
+  constructor(private rdata: DataService) {}
 
   ngOnInit(): void {
-    this.getresume()
-    this.getcurrentLocation()
+    this.getresume();
+    this.getcurrentLocation();
     this.like = false;
   }
 
-  getcurrentLocation(){
-    navigator.geolocation.getCurrentPosition((position) =>{
-        this.latitude = position.coords.latitude;
-        const latd = parseFloat(this.latitude);
-        this.longitude = position.coords.longitude;
-        // this.langtd = parseFloat(this.longitude)
-        console.log(position, this.latitude, latd)
-        let l = {...this.arrlglt[1], latd : this.latitude, langtd: this.longitude}
-        this.arrlglt[1] = l
-        l ? this.googleMap() : ''
-    })
+  getcurrentLocation() {
+    navigator.geolocation.getCurrentPosition((position) => {
+      this.latitude = position.coords.latitude;
+      const latd = parseFloat(this.latitude);
+      this.longitude = position.coords.longitude;
+      let l = {
+        ...this.arrlglt[1],
+        latd: this.latitude,
+        langtd: this.longitude,
+      };
+      this.arrlglt[1] = l;
+      l ? this.googleMap() : '';
+    });
   }
-  googleMap(){
+  googleMap() {
     let loader = new Loader({
-      apiKey: 'AIzaSyCtfYgcatV9vr5B5LhVWFE_2GFgI2IRU_Q'
-    })
+      apiKey: 'AIzaSyCtfYgcatV9vr5B5LhVWFE_2GFgI2IRU_Q',
+    });
     loader.load().then(() => {
-      this.map = new google.maps.Map(document.getElementById("map")!, {
+      this.map = new google.maps.Map(document.getElementById('map')!, {
         center: { lat: this.arrlglt[0].latd, lng: this.arrlglt[0].langtd },
         zoom: 20,
-        styles: styles
-      })
-      if(this.arrlglt.length){
-        for(let l of this.arrlglt){
+        styles: styles,
+      });
+      if (this.arrlglt.length) {
+        for (let l of this.arrlglt) {
           const marker = new google.maps.Marker({
             position: new google.maps.LatLng(l.latd, l.langtd),
             map: this.map,
             title: l.title,
             icon: {
-            url: l.icon,
-            scaledSize: new google.maps.Size(36, 36),
-            /**
-             * The position at which to anchor an image in correspondence to the
-             * location of the marker on the map. By default, the anchor is located
-             * along the center point of the bottom of the image.
-             */
-            anchor: new google.maps.Point(18, 18),
-            },           
+              url: l.icon,
+              scaledSize: new google.maps.Size(36, 36),
+              /**
+               * The position at which to anchor an image in correspondence to the
+               * location of the marker on the map. By default, the anchor is located
+               * along the center point of the bottom of the image.
+               */
+              anchor: new google.maps.Point(18, 18),
+            },
           });
           // Create an info window to share between markers.
           const infoWindow = new google.maps.InfoWindow();
-          marker.addListener("click", () => {
+          marker.addListener('click', () => {
             infoWindow.close();
             infoWindow.setContent(marker.getTitle());
             infoWindow.open(marker.getMap(), marker);
           });
         }
       }
-    })
-    
+    });
   }
-  share(){
-    const url = (window.location.href).toString()
+  share() {
+    const url = window.location.href.toString();
     const data = {
       title: 'portfolio',
       text: 'viswanath',
-      url: url
-    }
-    if(navigator.canShare(data) && navigator.canShare()){
-      navigator.share(data)
-    }else{
-      alert('sharing not supported for this device')
+      url: url,
+    };
+    if (navigator.canShare(data) && navigator.canShare()) {
+      navigator.share(data);
+    } else {
+      alert('sharing not supported for this device');
     }
   }
-  getresume(){
+  getresume() {
     this.rdata.getresume().subscribe((res: any) => {
-      for(const key in res){
-        this.resume = res[key]
-        console.log(res)
+      for (const key in res) {
+        this.resume = res[key];
       }
-    })
+    });
   }
 }
--- src/app/skills/skills.component.ts
+++ src/app/skills/skills.component.ts
@@ -1,42 +1,43 @@
 import { Component, OnInit } from '@angular/core';
-import data from '../data/data.json'
+import data from '../data/data.json';
 import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
-  styleUrls: ['./skills.component.css']
+  styleUrls: ['./skills.component.css'],
 })
 export class SkillsComponent implements OnInit {
-  skills: any
- 
+  skills: any;
+
   skillSets: any;
   nodataFound: any;
-  constructor(private data: DataService) { }
+  constructor(private data: DataService) {}
 
   ngOnInit(): void {
-    this.getresume()
+    this.getresume();
   }
 
   skillset(set: any) {
-    if(set == 1){
-      this.skillSets = this.skills.languages[0].web
-      this.nodataFound = this.skills.languages[0].web?.length 
-    } else if(set == 2){
-      this.skillSets = this.skills.languages[1].backend
-      this.skills.languages[1].backend ? this.nodataFound = this.skills.languages[1].backend?.length : this.nodataFound = 0
-    } else if(set == 3){
-      this.skillSets = this.skills.languages[2].testing
-      this.nodataFound = this.skills.languages[2].testing?.length
+    if (set == 1) {
+      this.skillSets = this.skills.languages[0].web;
+      this.nodataFound = this.skills.languages[0].web?.length;
+    } else if (set == 2) {
+      this.skillSets = this.skills.languages[1].backend;
+      this.skills.languages[1].backend
+        ? (this.nodataFound = this.skills.languages[1].backend?.length)
+        : (this.nodataFound = 0);
+    } else if (set == 3) {
+      this.skillSets = this.skills.languages[2].testing;
+      this.nodataFound = this.skills.languages[2].testing?.length;
     }
   }
-  getresume(){
+  getresume() {
     this.data.getresume().subscribe((res: any) => {
-      for(const key in res){
-        this.skills = res[key].aboutme.skills
-        console.log(this.skills, res[key].aboutme)
-        this.skills ? this.skillSets = this.skills.languages[0].web : ''
+      for (const key in res) {
+        this.skills = res[key].aboutme.skills;
+        this.skills ? (this.skillSets = this.skills.languages[0].web) : '';
       }
-    })
+    });
   }
 }
